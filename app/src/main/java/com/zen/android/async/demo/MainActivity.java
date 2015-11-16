b95d29ed0d2f22a41245fcc825dd2f5966681cdc
yangz <xggcl@sina.com>
2015-11-16 20:58:48 +0800

    add Thread Case

--- app/src/main/java/com/zen/android/async/demo/MainActivity.java
+++ app/src/main/java/com/zen/android/async/demo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.zen.android.async.demo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,6 +13,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.zen.android.async.demo.ui.ThreadCaseActivity;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -82,6 +85,7 @@ public class MainActivity extends AppCompatActivity
 
         if (id == R.id.nav_camera) {
             // Handle the camera action
+            startActivity(new Intent(this, ThreadCaseActivity.class));
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
